c6b5c945e0bd9e6c4281e0c2590dc6c3a5e51b85
Jableman19 <jableman@umich.edu>
2024-04-20 22:39:59 -0400

    Update key.h

--- game_engine/key.h
+++ game_engine/key.h
@@ -2,5 +2,5 @@
 #include <string>
 //define global api key
 namespace Key {
-	std::string OPENAI_KEY = "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";
+	std::string OPENAI_KEY = "YOUR_KEY_HERE";
 };
